06b2dfa3b4235391cd2aee0132f380c7e495b565
nanocloudx <nanocloudx@gmail.com>
2018-07-05 01:56:23 +0900

    update api

--- server/routes/index.mjs
+++ server/routes/index.mjs
@@ -1,116 +1,122 @@
 // import createError from 'http-errors'
 import express from 'express'
-import db from './database'
 import models from '../database/models'
+import uuidv4 from 'uuid/v4'
 
 const router = express.Router()
+
+// TODO エラーハンドリング書いてないので暇な時にちゃんと直す
 
 /**
  * イベント一覧
  */
-router.get('/api/events', (req, res, next) => {
-  databaseResponse(db.findAll(models.Event), res, next)
+router.get('/api/events', async (req, res, next) => {
+  const results = await models.Event.findAll()
+  databaseResponse(results, res)
 })
 
 /**
  * イベント情報
  */
-router.get('/api/events/:eventId', (req, res, next) => {
-  const schema = models.Event
-  const where = {
-    id: req.params.eventId
-  }
-  databaseResponse(db.findOne(schema, where), res, next)
+router.get('/api/events/:eventId', async (req, res, next) => {
+  const results = await models.Event.findById(req.params.eventId)
+  databaseResponse(results, res)
+})
+
+/**
+ * イベント登録
+ */
+router.post('/api/events', async (req, res, next) => {
+  const results = await models.Event.create({
+    id: req.body.id,
+    title: req.body.title,
+    place: req.body.place,
+    date: req.body.date
+  })
+  databaseResponse(results, res)
 })
 
 /**
  * プレーヤー一覧
  */
-router.get('/api/players', (req, res, next) => {
-  databaseResponse(db.findAll(models.Player), res, next)
+router.get('/api/players', async (req, res, next) => {
+  const results = await models.Player.findAll()
+  databaseResponse(results, res)
 })
 
 /**
  * プレーヤー情報
  */
-router.get('/api/players/:playerId', (req, res, next) => {
-  const schema = models.Player
-  const where = {
-    id: req.params.playerId
-  }
-  databaseResponse(db.findOne(schema, where), res, next)
+router.get('/api/players/:playerId', async (req, res, next) => {
+  const results = await models.Player.findById(req.params.playerId)
+  databaseResponse(results, res)
+})
+
+/**
+ * プレーヤー登録
+ */
+router.post('/api/players', async (req, res, next) => {
+  const results = await models.Player.create({
+    id: uuidv4(),
+    organization: req.body.organization,
+    name: req.body.name
+  })
+  databaseResponse(results, res)
 })
 
 /**
  * イベント参加者一覧
  */
 router.get('/api/events/:eventId/players', async (req, res, next) => {
-
-  // TODO リレーション実装ができないと一覧取得できない
-
-  // const schema = models.EventPlayerRelation
-  // const where = {
-  //   EventId: req.params.eventId
-  // }
-  // databaseResponse(db.findAll(schema, where), res, next)
+  const results = await models.Event.findOne({
+    include: [{
+      model: models.Player,
+      through: { attributes: ['rank', 'status'] }
+    }],
+    where: { id: req.params.eventId }
+  })
+  databaseResponse(results, res)
 })
 
 /**
  * イベント参加登録
  */
-router.post('/api/events/:eventId/players/:playerId', (req, res, next) => {
-  const schema = models.EventPlayerRelation
-  const context = {
+router.post('/api/events/:eventId/players/:playerId', async (req, res, next) => {
+  const results = await models.EventPlayerRelation.create({
+    id: uuidv4(),
     EventId: req.params.eventId,
     PlayerId: req.params.playerId
-  }
-  databaseResponse(db.create(schema, context), res, next)
+  })
+  databaseResponse(results, res)
 })
 
 /**
  * イベント参加情報
  */
 router.get('/api/events/:eventId/players/:playerId', async (req, res, next) => {
-
-  // TODO この実装は良くない
-
-  const EventPlayerRelation = await db.findOne(models.EventPlayerRelation, {
-    EventId: req.params.eventId,
-    PlayerId: req.params.playerId
+  const results = await models.Event.findOne({
+    include: [{
+      model: models.Player,
+      through: { attributes: ['rank', 'status'] },
+      where: { id: req.params.playerId }
+    }],
+    where: { id: req.params.eventId }
   })
-  const Player = await db.findOne(models.Player, {
-    id: req.params.playerId
-  })
-  if (!EventPlayerRelation || !Player) {
-    failureResponse(res)
-    return
-  }
-  const result = {
-    id: Player.id,
-    organization: Player.organization,
-    name: Player.name,
-    rank: EventPlayerRelation.rank,
-    status: EventPlayerRelation.rank
-  }
-  databaseResponse(Promise.resolve(result), res, next)
+  databaseResponse(results, res)
 })
 
 /**
  * ゲーム開始
  */
-router.put('/api/events/:eventId/players/:playerId/active', async (req, res, next) => {
-  const schema = models.EventPlayerRelation
-  const where = {
-    EventId: req.params.eventId,
-    PlayerId: req.params.playerId
-  }
-  const context = {
-    status: 'active'
-  }
-  const result = await db.update(schema, where, context)
+router.put('/api/events/:eventId/players/:playerId/playing', async (req, res, next) => {
+  const result = await models.EventPlayerRelation.update({ status: 'playing' }, {
+    where: {
+      EventId: req.params.eventId,
+      PlayerId: req.params.playerId
+    }
+  })
   if (result[0] === 0) {
-    failureResponse(res)
-    return
+    return failureResponse(res)
   }
   successResponse(res)
 })
@@ -119,38 +125,30 @@ router.put('/api/events/:eventId/players/:playerId/active', async (req, res, nex
  * ゲーム終了
  */
 router.put('/api/events/:eventId/players/:playerId/finish', async (req, res, next) => {
-  const schema = models.EventPlayerRelation
-  const where = {
-    EventId: req.params.eventId,
-    PlayerId: req.params.playerId
-  }
-  const context = {
-    status: 'finish'
-  }
-  const result = await db.update(schema, where, context)
+  const result = await models.EventPlayerRelation.update({ status: 'finish' }, {
+    where: {
+      EventId: req.params.eventId,
+      PlayerId: req.params.playerId
+    }
+  })
   if (result[0] === 0) {
-    failureResponse(res)
-    return
+    return failureResponse(res)
   }
   successResponse(res)
 })
 
 /**
- * ゲーム中止
+ * ゲーム未参加
  */
-router.put('/api/events/:eventId/players/:playerId/cancel', async (req, res, next) => {
-  const schema = models.EventPlayerRelation
-  const where = {
-    EventId: req.params.eventId,
-    PlayerId: req.params.playerId
-  }
-  const context = {
-    status: 'none'
-  }
-  const result = await db.update(schema, where, context)
+router.put('/api/events/:eventId/players/:playerId/none', async (req, res, next) => {
+  const result = await models.EventPlayerRelation.update({ status: 'none' }, {
+    where: {
+      EventId: req.params.eventId,
+      PlayerId: req.params.playerId
+    }
+  })
   if (result[0] === 0) {
-    failureResponse(res)
-    return
+    return failureResponse(res)
   }
   successResponse(res)
 })
@@ -162,8 +160,9 @@ router.get(['/'], (req, res, next) => {
   res.render('index')
 })
 
-function databaseResponse (dbRequest, res, next) {
-  dbRequest.then(results => { res.json(results) }).catch(err => { next(err) })
+function databaseResponse (results, res) {
+  res.status(200)
+  res.json(results)
 }
 function successResponse (res) {
   res.status(200)
